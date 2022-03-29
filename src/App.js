8867ec0605da880e00b3240b9c9982f6145d22b4
Lena Hou <lenahou2013@gmail.com>
2022-03-29 18:39:44 -0400

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
-      {cards}
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 function Card(props){
-    // console.log(props)
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === 'Online') {
+        badgeText = "ONLINE"
+    }
+
     return(
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={`../images/${props.coverImg}`} alt="" className="card--thumbnail" />
             <div className="card--stats">
-               <img src="../images/start-icon.png" className="card--star" alt="" />
+               <img src="../images/star-icon.png" className="card--star" alt="" />
                <span className="grey">{props.rating}</span>
                <span className="grey">({props.reviewCount}) •</span>
                <span className="grey">{props.location}</span>
